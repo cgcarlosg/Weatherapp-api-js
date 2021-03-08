9f41eb76551bfeb37fc5d040fcaac583ae6170c1
carlos <cgcarlosg@hotmail.com>
2021-03-08 12:38:43 -0500

    add functions for the API

--- src/index.js
+++ src/index.js
@@ -1,20 +1,26 @@
-import _ from 'lodash';
-import './style.css';
-import Icon from './icon.png';
-
-function component() {
-  const element = document.createElement('div');
-
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-  // Add the image to our existing div.
-  const myIcon = new Image();
-  myIcon.src = Icon;
-
-  element.appendChild(myIcon);
-
-  return element;
+const api = {
+    key: "f56dca24ce05c3835045624436d6a8e8",
+    base: "https://api.openweathermap.org/data/2.5"
 }
 
-document.body.appendChild(component());
+const searchbox = document.querySelector('.search-box');
+searchbox.addEventListener('keypress', setQuery);
+
+function setQuery(evt) {
+    if (evt.keyCode == 13) {
+//        getResults(searchbox.value);
+        console.log(searchbox.value);
+        searchbox.value = '';
+    }
+}
+
+function getResults (query) {
+    fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+    .then(weather => {
+        return weather.json();
+    }).then(displayResults);
+}
+
+function displayResults (weather) {
+    console.log(weather);
+}
