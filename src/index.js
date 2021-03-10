7128434662adbe544398eb8c92a29e6dc04c3ad8
carlos <cgcarlosg@hotmail.com>
2021-03-10 12:54:09 -0500

    Add display functions and fix linters

--- src/index.js
+++ src/index.js
@@ -1,42 +1,49 @@
 const api = {
-    key: "a012917c99ef51c392b259e302401f8c",
-    base: "https://api.openweathermap.org/data/2.5/"
-}
+  key: 'a012917c99ef51c392b259e302401f8c',
+  base: 'https://api.openweathermap.org/data/2.5/',
+};
 
 const searchbox = document.querySelector('.search-box');
-searchbox.addEventListener('keypress', setQuery);
+
+function dateBuilder(d) {
+  const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  const days = ['Sun', 'Mon', 'Tues', 'Wed', 'thurs', 'Fri', 'Sat'];
+
+  const day = days[d.getDay()];
+  const date = d.getDate();
+  const month = months[d.getMonth()];
+  const year = d.getFullYear();
+
+  return `${day} ${date} ${month} ${year}`;
+}
+
+function displayResults(weather) {
+  const city = document.querySelector('.location .city');
+  city.innerText = `${weather.name}, ${weather.sys.country}`;
+
+  const now = new Date();
+  const date = document.querySelector('.location .date');
+  date.innerText = dateBuilder(now);
+
+  const temp = document.querySelector('.current .temp');
+  temp.innerHTML = `${Math.round(weather.main.temp)}<span>°C</span>`;
+
+  const weatherEl = document.querySelector('.current .weather');
+  weatherEl.innerText = weather.weather[0].main;
+
+  const hilow = document.querySelector('.hi-low');
+  hilow.innerText = `${Math.round(weather.main.temp_min)}°C / ${Math.round(weather.main.temp_max)}°C`;
+}
+
+function getResults(query) {
+  fetch(`${api.base}weather?q=${query}&units=metric&appid=${api.key}`)
+    .then(weather => weather.json()).then(displayResults);
+}
 
 function setQuery(evt) {
-    if (evt.keyCode == 13) {
-        getResults(searchbox.value);
-    }
+  if (evt.keyCode === 13) {
+    getResults(searchbox.value);
+  }
 }
 
-function getResults (query) {
-    fetch(`${api.base}weather?q=${query}&units=metric&appid=${api.key}`)
-    .then(weather => {
-        return weather.json();
-    }).then(displayResults);
-}
-
-function displayResults (weather) {
-    console.log(weather);
-    const city = document.querySelector('.location .city');
-    city.innerText = `${weather.name}, ${weather.sys.country}`;
-
-    const now = new Date ();
-    const date = document.querySelector('.location .date');
-    date.innerText = dateBuilder(now);
-}
-
-function dateBuilder (d) {
-    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    const days = ["Sun", "Mon", "Tues", "Wed", "thurs", "Fri", "Sat"];
-
-    const day = days[d.getDay()];
-    const date = d.getDate();
-    const month = months[d.getMonth()];
-    const year = d.getFullYear();
-
-    return `${day} ${date} ${month} ${year}`
-}
+searchbox.addEventListener('keypress', setQuery);
